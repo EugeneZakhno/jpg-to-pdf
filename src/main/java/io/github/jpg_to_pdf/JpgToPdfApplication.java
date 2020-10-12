e1bf2c675fb7ef3f5cc5fbed06bc25cd77984afb
EugeneZakhno <zohan1234567@gmail.com>
2020-10-12 22:57:02 +0300

    init

---
+++ src/main/java/io/github/jpg_to_pdf/JpgToPdfApplication.java
@@ -0,0 +1,13 @@
+package io.github.jpg_to_pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpgToPdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpgToPdfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/jpg_to_pdf/JpgToPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jpg_to_pdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpgToPdfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
