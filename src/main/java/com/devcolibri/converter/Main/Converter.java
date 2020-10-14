8bcca7b5602f156d21bfbcb015dc6432a26891c4
EugeneZakhno <zohan1234567@gmail.com>
2020-10-14 23:00:59 +0300

    added new realization

--- src/main/java/com/devcolibri/converter/Main/Converter.java
+++ src/main/java/com/devcolibri/converter/Main/Converter.java
@@ -10,15 +10,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Converter {
-
-    public static void main(String... args) throws IOException, DocumentException {
+    public static void main(String [] args) throws IOException, DocumentException {
         Document document = new Document();
         PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("fail.pdf"));
         document.open();
         XMLWorkerHelper.getInstance().parseXHtml(writer, document, new FileInputStream("index.html"));
         document.close();
-
         System.out.println("Your pdf fail is created!");
     }
-
 }
